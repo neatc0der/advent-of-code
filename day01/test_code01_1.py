c084ca00a422d68bddd650b8e74ed6b92a5bc489
neatc0der <grosch.tobias@googlemail.com>
2021-12-01 23:13:25 +0100

    Fixes unit tests for code01_1

--- day01/test_code01_1.py
+++ day01/test_code01_1.py
@@ -2,7 +2,7 @@
 from typing import List
 from unittest import main, TestCase
 
-from code01_1 import solve01_1
+from code01_1 import solve
 
 
 class Tests01_1(TestCase):
@@ -27,32 +27,32 @@ class Tests01_1(TestCase):
             if line.strip() != ""
         )
 
-        result: Dict[str, int] = solve01_1(example, key_inc="inc")
+        result: Dict[str, int] = solve(example, key_inc="inc")
         self.assertEqual(result["inc"], example_result)
 
     def test_empty_contains_no_increase(self) -> None:
-        result: Dict[str, int] = solve01_1([], key_inc="inc")
+        result: Dict[str, int] = solve([], key_inc="inc")
         self.assertEqual(result["inc"], 0)
 
     def test_single_line_contains_no_increase(self) -> None:
-        result: Dict[str, int] = solve01_1([123], key_inc="inc")
+        result: Dict[str, int] = solve([123], key_inc="inc")
         self.assertEqual(result["inc"], 0)
 
     def test_equal_lines_contain_no_increase(self) -> None:
-        result: Dict[str, int] = solve01_1([1, 1], key_inc="inc")
+        result: Dict[str, int] = solve([1, 1], key_inc="inc")
         self.assertEqual(result["inc"], 0)
 
     def test_decrease_lines_contain_no_increase(self) -> None:
-        result: Dict[str, int] = solve01_1([2, 1], key_inc="inc")
+        result: Dict[str, int] = solve([2, 1], key_inc="inc")
         self.assertEqual(result["inc"], 0)
 
     def test_increase_lines_contain_one_increase(self) -> None:
-        result: Dict[str, int] = solve01_1([1, 2], key_inc="inc")
+        result: Dict[str, int] = solve([1, 2], key_inc="inc")
         self.assertEqual(result["inc"], 1)
 
     def test_invalid_line_forces_exception(self) -> None:
         with self.assertRaises(RuntimeError):
-            result: Dict[str, int] = solve01_1(["not a number"], key_inc="inc")
+            result: Dict[str, int] = solve(["not a number"], key_inc="inc")
 
 
 if __name__ == "__main__":
